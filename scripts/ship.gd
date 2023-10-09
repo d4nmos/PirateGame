2879e40205ba4ffdce1f90020e271f0545f84ce4
darikzen <Darikzen@gmail.com>
2023-10-09 20:37:33 +0700

    attempt to make ship rotation

--- scripts/ship.gd
+++ scripts/ship.gd
@@ -13,13 +13,18 @@ func _physics_process(delta):
 		var input_dir = Input.get_vector("left", "right", "forward", "backward")
 		var direction = Vector3.FORWARD	#	(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
-		if Input.is_action_pressed("right"):
-			direction = direction.rotated(Vector3.UP, -delta * ROTATIONSPEED)
-		if Input.is_action_pressed("left"):
-			direction = direction.rotated(Vector3.UP, delta * ROTATIONSPEED)
+#		if Input.is_action_pressed("right"):
+#			direction.x += 1
+#		if Input.is_action_pressed("left"):
+#			direction.z -= 1
 		
-		var translation = direction * SPEED * delta	
+		var translation = direction * SPEED * delta
 		translate(translation)
+		
+#		var angle = -direction.x * ROTATIONSPEED * delta
+#		var axis = Vector3.UP
+#		rotate(axis, angle)
+
 
 
 func _on_control_area_body_entered(body):
