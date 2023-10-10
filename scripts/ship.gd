e7c083ffcbdc0f3dfab7f257a65e83c06de986ff
darikzen <Darikzen@gmail.com>
2023-10-10 14:05:59 +0700

    add ship acceleration control

--- scripts/ship.gd
+++ scripts/ship.gd
@@ -2,24 +2,30 @@ extends AnimatableBody3D
 
 @onready var visuals = $visuals
 
-const SPEED = 1.0
-const ROTATIONSPEED = 10.0
+var speed = 0.0
+const ACCELIRATION = 0.005
+var rotationSpeed = 10.0
 
 func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Vector3.FORWARD
 	if Globals.controlShip:
 		var input_dir = Input.get_vector("left", "right", "forward", "backward")
-		var direction = Vector3.FORWARD	#	(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
+			#	(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		
+		if Input.is_action_pressed("forward"): 
+			speed += ACCELIRATION
+		if Input.is_action_pressed("backward") and speed - ACCELIRATION >= 0: 
+			speed -= ACCELIRATION
 #		if Input.is_action_pressed("right"):
 #			direction.x += 1
 #		if Input.is_action_pressed("left"):
 #			direction.z -= 1
 		
-		var translation = direction * SPEED * delta
-		translate(translation)
+	var translation = direction * speed * delta
+	translate(translation)
 		
 #		var angle = -direction.x * ROTATIONSPEED * delta
 #		var axis = Vector3.UP
