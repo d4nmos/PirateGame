63037c128e53580ff14dbdf86515dfb6f9bae661
d4nmos <danmoswork@gmail.com>
2023-10-11 18:31:03 +0800

    Add usefull hp bar

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,10 +1,16 @@
 extends CharacterBody3D
 
-@export var hp = 5.0
+@export var max_hp_value = 5
+var hp
 
+func _ready():
+	hp = max_hp_value
+	
 func take_damage(damage):
 	hp -= damage
-	print(hp)	
+	
+	var health_bar_reducer = (damage * 100) / max_hp_value
+	$health_bar_3D/SubViewport/healthBar2D.value -= health_bar_reducer
 	
 	if hp <= 0:
 		queue_free()
