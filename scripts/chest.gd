8ecdad7e5f43ae85a8f9c027f8f2a006d2d8c42a
d4nmos <danmoswork@gmail.com>
2024-01-30 18:01:27 +0800

    Added visual selection of objects

--- scripts/chest.gd
+++ scripts/chest.gd
@@ -1,3 +1,10 @@
-extends AnimatableBody3D
+extends StaticBody3D
 
-	
+func _ready():
+	pass
+
+func chose_this_object():
+	$interact_arrow_3D.visible = true
+
+func unchose_this_object():
+	$interact_arrow_3D.visible = false
--- scripts/interaction_manager.gd
+++ scripts/interaction_manager.gd
@@ -8,24 +8,32 @@ var current_object = null
 func add_interactable_object(object):
 	interactable_objects.append(object)
 	interaction_pointer += 1
-	current_object = interactable_objects[interaction_pointer]
+	change_current_object(interaction_pointer)
 
 func remove_interactable_object(object):
 	interactable_objects.erase(object)
 	
 	if interactable_objects.is_empty():
 		interaction_pointer = -1
+		current_object.unchose_this_object()
 		current_object = null
 	else:
 		interaction_pointer = 0
-		current_object = interactable_objects[interaction_pointer] 
+		change_current_object(interaction_pointer) 
 	
 func up_pointer():
 	if not interactable_objects.is_empty() and interaction_pointer < interactable_objects.size() - 1:
 		interaction_pointer += 1
-		current_object = interactable_objects[interaction_pointer]
+		change_current_object(interaction_pointer)
 	
 func down_pointer():
 	if not interactable_objects.is_empty() and interaction_pointer > 0:
 		interaction_pointer -= 1
-		current_object = interactable_objects[interaction_pointer]
+		change_current_object(interaction_pointer)
+
+func change_current_object(pointer):
+	if current_object != null:
+		current_object.unchose_this_object()
+		
+	current_object = interactable_objects[pointer]
+	current_object.chose_this_object()
