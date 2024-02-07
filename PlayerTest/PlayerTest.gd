a3f7c686c79cdcb4e9cbd0ec3af10be12e050375
HeroInTheBox <a02polina@gmail.com>
2024-02-07 23:01:29 +0800

    Update PlayerTest.gd

--- PlayerTest/PlayerTest.gd
+++ PlayerTest/PlayerTest.gd
@@ -6,7 +6,7 @@ extends CharacterBody3D
 @onready var bird_cam = $Cam2
 @onready var normal_cam = $Cam/V/Cam1
 @export var JUMP_VELOCITY = 6.5
-@export var SPEED = 20
+@export var SPEED = 500
 var mouse_input = Vector2()
 var move_vector = Vector3()
 @export var gravity_enabled = false 
@@ -24,9 +24,9 @@ func _physics_process(delta):
 	#if not is_on_floor() and gravity_enabled:
 		#velocity.y -= gravity * delta
 		
-	var vertical_move = Input.get_axis("crouch","jump")
-	if gravity_enabled==false:
-		velocity.y = vertical_move * SPEED
+	#var vertical_move = Input.get_axis("crouch","jump")
+	#if gravity_enabled==false:
+	#	velocity.y = vertical_move * SPEED
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -34,7 +34,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (cam.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -44,7 +44,7 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-	
+	"""
 	#Camera Control
 	cam.rotation.y -= mouse_input.x * delta
 	cam_v.rotation.x -= mouse_input.y * delta
@@ -61,7 +61,7 @@ func _physics_process(delta):
 		else:
 			bird_cam.current = true
 	mouse_input = Vector2()
-	
+	"""
 	
 
 func _input(event):
