e03892edcec66c1e480411e6f0a598e08862197a
d4nmos <danmoswork@gmail.com>
2024-04-15 22:30:27 +0800

    Finish grid_generator

--- scripts/grid_generator.gd
+++ scripts/grid_generator.gd
@@ -1,14 +1,14 @@
 extends Node3D
 
-@onready var cell = preload("res://scenes/cell.tscn")
+@onready var cell_scn = preload("res://scenes/cell.tscn")
 @onready var empty_box = $Empty_box
 
-@export var matrix_size = 5
+@export var matrix_size = 3
 
 var _cell_x_scale
 var _cell_z_scale
 var _grid_matrix
-var _empty_cell
+var _stack: Array = []
 
 func _ready():
 	_grid_matrix = create_matrix(matrix_size)
@@ -16,98 +16,31 @@ func _ready():
 
 
 func create_grid():
-	var start_row = get_middle_matrix_pos()
-	var start_col = get_middle_matrix_pos()
-	var start_cell_pos = empty_box.global_transform.origin
+	var start_pos = empty_box.global_transform.origin
+	var middle_matrix_pos = get_middle_matrix_pos()
 	get_cell_scale()
-#	create_neighbors(start_row, start_col, start_cell_pos)
-	test_create_neighbors(start_row, start_col, start_cell_pos)
 	
-func create_neighbors(row, col, cell_pos):
-	_grid_matrix[row][col] = 1
-	var cellInstance = cell.instantiate()
-	add_child(cellInstance)
-	cellInstance.global_transform.origin = cell_pos
+	start_pos.x -= _cell_x_scale * middle_matrix_pos
+	start_pos.z -= _cell_z_scale * middle_matrix_pos
 	
-	if row - 1 >= 0:
-		if _grid_matrix[row - 1][col] == 0:
-			create_neighbors(row - 1, col, get_forward_pos(cell_pos))
+	create_neighbors(start_pos)
+
+func create_neighbors(start_pos):
+	var pos = start_pos
 	
-	if col + 1 < matrix_size:
-		if _grid_matrix[row][col + 1] == 0:
-			create_neighbors(row, col + 1, get_right_pos(cell_pos))
-	
-	if row + 1 < matrix_size:
-		if _grid_matrix[row + 1][col] == 0:
-			create_neighbors(row + 1, col, get_backward_pos(cell_pos))
-	
-	if col - 1 >= 0:
-		if _grid_matrix[row][col - 1] == 0:
-			create_neighbors(row, col - 1, get_left_pos(cell_pos))
-
-func test_create_neighbors(start_row, start_col, start_cell_pos):
-	var stack = []
-	_empty_cell = cell.instantiate()
-	_empty_cell.row = start_row
-	_empty_cell.col = start_col
-	_empty_cell.pos = start_cell_pos
-	stack.append(_empty_cell)
-	
-	while stack.size() > 0:
-		var current = stack.pop_back()
-		var row = current.row
-		var col = current.col
-		var cell_pos = current.pos
-		
-		_grid_matrix[row][col] = 1
-		var cellInstance = cell.instantiate()
-		add_child(cellInstance)
-		cellInstance.global_transform.origin = cell_pos
-		
-		if row - 1 >= 0 and _grid_matrix[row - 1][col] == 0:
-			_empty_cell.row = row - 1
-			_empty_cell.col = col
-			_empty_cell.pos = get_forward_pos(cell_pos)
-			stack.append(_empty_cell)
-		
-		if col + 1 < matrix_size and _grid_matrix[row][col + 1] == 0:
-			_empty_cell.row = row
-			_empty_cell.col = col + 1
-			_empty_cell.pos = get_right_pos(cell_pos)
-			stack.append(_empty_cell)
-		
-		if row + 1 < matrix_size and _grid_matrix[row + 1][col] == 0:
-			_empty_cell.row = row + 1
-			_empty_cell.col = col
-			_empty_cell.pos = get_backward_pos(cell_pos)
-			stack.append(_empty_cell)
-		
-		if col - 1 >= 0 and _grid_matrix[row][col - 1] == 0:
-			_empty_cell.row = row
-			_empty_cell.col = col - 1
-			_empty_cell.pos = get_left_pos(cell_pos)
-			stack.append(_empty_cell)
-
-
-func get_forward_pos(original_pos):
-	var new_pos = original_pos
-	new_pos.z -= _cell_z_scale
-	return new_pos
-
-func get_backward_pos(original_pos):
-	var new_pos = original_pos
-	new_pos.z += _cell_z_scale
-	return new_pos
-
-func get_right_pos(original_pos):
-	var new_pos = original_pos
-	new_pos.x += _cell_x_scale
-	return new_pos
-
-func get_left_pos(original_pos):
-	var new_pos = original_pos
-	new_pos.x -= _cell_x_scale
-	return new_pos
+	for i in range(matrix_size):
+		for j in range(matrix_size):
+			_grid_matrix[i][j] = 1
+			var new_cell = cell_scn.instantiate()
+			new_cell.row = i
+			new_cell.col = j
+			add_child(new_cell)
+			
+			new_cell.global_transform.origin = pos
+			pos.x += _cell_x_scale
+			
+		pos.x = start_pos.x
+		pos.z += _cell_z_scale
 
 func create_matrix(rows_and_cols):
 	var matrix = []
@@ -122,7 +55,7 @@ func get_middle_matrix_pos():
 	return matrix_size / 2
 
 func get_cell_scale():
-	var cellInstance = cell.instantiate()
+	var cellInstance = cell_scn.instantiate()
 	add_child(cellInstance)
 	_cell_x_scale = cellInstance.scale.x
 	_cell_z_scale = cellInstance.scale.z
