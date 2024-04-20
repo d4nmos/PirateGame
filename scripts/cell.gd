cb66c249bde6448129bf8c0c27df3d37432a14fd
d4nmos <danmoswork@gmail.com>
2024-04-20 19:06:48 +0800

    Added pathfinding system

--- scripts/cell.gd
+++ scripts/cell.gd
@@ -1,19 +1,44 @@
 extends Node3D
 
 @onready var cell_area = $Cell_Area
-@onready var ray_cast_3d = $RayCast3D
+@onready var start_marker = preload("res://scenes/test/simple_start_marker.tscn")
+@onready var finish_marker = preload("res://scenes/test/simple_finish_marker.tscn")
+@onready var path_market = preload("res://scenes/test/path_marker.tscn")
 
 var col
 var row
-var pos
+var count = 0
+var is_passability = true
+var previous
+
+var _grid_generator
 
 func _ready():
-	pass
+	_grid_generator = get_parent()
+	
+	
 
 func _physics_process(delta):
 	if cell_area.has_overlapping_bodies():
 		self.global_transform.origin.y += 0.1
-	if ray_cast_3d.is_colliding():
-		var collision_point = ray_cast_3d.get_collision_point()
-		var distance = ray_cast_3d.global_transform.origin.distance_to(collision_point)
-		
+		count += 0.1 
+		if count > 1:
+			_grid_generator.grid_matrix[row][col].is_passability = false
+			queue_free()
+
+func set_as_start():
+	var start = start_marker.instantiate()
+	add_child(start)
+	start.global_transform.origin.y += 0.2
+
+func set_as_finish():
+	var finish = finish_marker.instantiate()
+	add_child(finish)
+	finish.global_transform.origin.y += 0.2
+
+func set_as_path():
+	var path = path_market.instantiate()
+	add_child(path)
+	path.global_transform.origin.y += 1
+	
+	
--- scripts/grid_generator.gd
+++ scripts/grid_generator.gd
@@ -7,13 +7,18 @@ extends Node3D
 
 var _cell_x_scale
 var _cell_z_scale
-var _grid_matrix
-var _stack: Array = []
+var _start_cell
+var _finish_cell
+
+var grid_matrix
 
 func _ready():
-	_grid_matrix = create_matrix(matrix_size)
+	grid_matrix = create_matrix(matrix_size)
 	create_grid()
-
+	set_random_start()
+	set_random_finish()
+	
+	find_path()
 
 func create_grid():
 	var start_pos = empty_box.global_transform.origin
@@ -30,11 +35,11 @@ func create_neighbors(start_pos):
 	
 	for i in range(matrix_size):
 		for j in range(matrix_size):
-			_grid_matrix[i][j] = 1
 			var new_cell = cell_scn.instantiate()
 			new_cell.row = i
 			new_cell.col = j
 			add_child(new_cell)
+			grid_matrix[i][j] = new_cell
 			
 			new_cell.global_transform.origin = pos
 			pos.x += _cell_x_scale
@@ -60,3 +65,106 @@ func get_cell_scale():
 	_cell_x_scale = cellInstance.scale.x
 	_cell_z_scale = cellInstance.scale.z
 	cellInstance.queue_free()
+	
+func set_random_start():
+	var row = randi_range(0, matrix_size - 1)
+	var col = randi_range(0, matrix_size - 1)
+	if grid_matrix[row][col].is_passability:
+		grid_matrix[row][col].set_as_start()
+		_start_cell = Vector2(row, col)
+		return
+	else:
+		set_random_start()
+
+func set_random_finish():
+	var row = randi_range(0, matrix_size - 1)
+	var col = randi_range(0, matrix_size - 1)
+	if grid_matrix[row][col].is_passability:
+		grid_matrix[row][col].set_as_finish()
+		_finish_cell = Vector2(row, col)
+		return
+	else:
+		set_random_finish()
+
+func find_path():
+	var reachable = [_start_cell]
+	var explored = []
+	
+	while !reachable.is_empty():
+		var cell = choose_cell(reachable)
+		
+		if cell == _finish_cell:
+			show_path(build_path(_finish_cell))
+			
+		
+		reachable.erase(cell)
+		explored.append(cell)
+		
+		var new_reachable = get_adjacent_nodes(cell, explored)
+		for adjacent in new_reachable:
+			if adjacent not in reachable:
+				get_cell_node_from_grid_matrix(adjacent).previous = cell
+				reachable.append(adjacent)
+		
+func choose_cell(reachable):
+	var min_distance = 1.79769e+308
+	var chosen_cell
+	var finish = get_cell_node_from_grid_matrix(_finish_cell)
+	var current_cell
+	
+	for cell in reachable:
+		current_cell = get_cell_node_from_grid_matrix(cell)
+		var current_distance = get_distance_to(current_cell, finish)
+		
+		if current_distance < min_distance:
+			min_distance = current_distance
+			chosen_cell = cell
+			
+	return chosen_cell
+
+func get_cell_node_from_grid_matrix(cell_row_col):
+	return grid_matrix[cell_row_col.x][cell_row_col.y]
+
+func get_distance_to(first_cell, second_cell):
+	var distance = first_cell.global_transform.origin.distance_to(second_cell.global_transform.origin)
+	return distance
+
+func build_path(finish):
+	var path = []
+	var to_node = finish
+	while to_node != null:
+		path.append(to_node)
+		var next_cell = get_cell_node_from_grid_matrix(to_node)
+		to_node = next_cell.previous
+	
+	return path
+
+func get_adjacent_nodes(node, explored):
+	var row = node.x
+	var col = node.y
+	var adjacent = []
+	
+	if col - 1 >= 0:
+		if grid_matrix[row][col - 1].is_passability and Vector2(row, col - 1) not in explored:
+			adjacent.append(Vector2(row, col - 1))
+	
+	if col + 1 < matrix_size:
+		if grid_matrix[row][col + 1].is_passability and Vector2(row, col + 1) not in explored:
+			adjacent.append(Vector2(row, col + 1))
+	
+	if row - 1 >= 0:
+		if grid_matrix[row - 1][col].is_passability and Vector2(row - 1, col) not in explored:
+			adjacent.append(Vector2(row - 1, col))
+			
+	if row + 1 < matrix_size:
+		if grid_matrix[row + 1][col].is_passability and Vector2(row + 1, col) not in explored:
+			adjacent.append(Vector2(row + 1, col))
+	
+	return adjacent
+
+func show_path(path):
+	for cell in path:
+		get_cell_node_from_grid_matrix(cell).set_as_path()
+	
+	
+
