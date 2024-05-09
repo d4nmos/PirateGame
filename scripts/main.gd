162ab5636f0c050bf4caf74192cc6d969deec9c1
HeroInTheBox <a02polina@gmail.com>
2024-05-09 14:43:11 +0800

    Update main.gd

--- scripts/main.gd
+++ scripts/main.gd
@@ -12,8 +12,8 @@ const PickUp = preload("res://inventory/items/pick_up/pick_up.tscn")
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	SimpleGrass.set_interactive(true)
-	#player.toggle_inventory.connect(toggle_iventory_interface)
-	#inventory_interface.set_player_inventory_data(player.inventory_data)
+	player.toggle_inventory.connect(toggle_iventory_interface)
+	inventory_interface.set_player_inventory_data(player.inventory_data)
 	
 	for node in get_tree().get_nodes_in_group("ExternalInventory"):
 		node.toggle_inventory.connect(toggle_iventory_interface)
