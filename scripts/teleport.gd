6a33ef9a623207833da53425ab1c120accc0294f
HeroInTheBox <a02polina@gmail.com>
2024-05-09 12:05:08 +0800

    Update teleport.gd

--- scripts/teleport.gd
+++ scripts/teleport.gd
@@ -4,7 +4,3 @@ var start_level: PackedScene
 
 func _ready():
 	start_level = preload("res://scenes/location1.tscn")
-
-#func _on_Area_entered(area: Area) -> void:
-#	if area.is_in_group("player"):
-#		get_tree().change_scene_to_packed(start_level)
