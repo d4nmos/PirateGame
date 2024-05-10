56eb41d4c0c9bb169907efb79510cd309ce89796
HeroInTheBox <a02polina@gmail.com>
2024-05-10 17:53:41 +0800

    Create dialog_manager.gd

---
+++ scripts/dialog_manager.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+@export var next_button : PackedScene
+
+var dialogue : Dialogue:
+	set(value):
+		dialogue = value
+		
+		%UI_dialog/PanelContainer/VBoxContainer/HBoxContainer/Icon.texture = value.texture
+		%UI_dialog/PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/Name.text = value.name
+		%UI_dialog/PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/Dialogue.text = value.dialogue
+		
+		reset_options()
+		add_buttons(value.options)
+		
+		await get_tree().create_timer(0.5).timeout
+		%UI_dialog/PanelContainer/VBoxContainer/Options.show()
+
+
+#func _ready():
+#	dialogue = load("res://NPCs/Pirate/Dialogues/0.tres")
+	
+
+func reset_options():
+	for child in %UI_dialog/PanelContainer/VBoxContainer/Options.get_children():
+		child.queue_free()
+	%UI_dialog/PanelContainer/VBoxContainer/Options.hide()
+
+func add_buttons(options):
+	for option in options:
+		var button = next_button.instantiate()
+		button.dialogue = option
+		%UI_dialog/PanelContainer/VBoxContainer/Options.add_child(button)
+		
+func hide_dialogue():
+	%UI_dialog.hide()
+	
+func show_dialogue():
+	%UI_dialog.show()
+
+		
+		
+		
