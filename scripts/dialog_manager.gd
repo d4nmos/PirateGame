179da448e8f844d094c373ce565aabfe3384706c
HeroInTheBox <a02polina@gmail.com>
2024-05-10 22:55:25 +0800

    Update dialog_manager.gd

--- scripts/dialog_manager.gd
+++ scripts/dialog_manager.gd
@@ -1,20 +1,30 @@
 extends Node2D
 
 @export var next_button : PackedScene
+@export var quest_button : PackedScene
 
 var dialogue : Dialogue:
 	set(value):
 		dialogue = value
+		
+		if current_speaker:
+			current_speaker.dialogue = value
 		
 		%UI_dialog/PanelContainer/VBoxContainer/HBoxContainer/Icon.texture = value.texture
 		%UI_dialog/PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/Name.text = value.name
 		%UI_dialog/PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/Dialogue.text = value.dialogue
 		
 		reset_options()
+		
+		if value.quest:
+			add_quest(value.quest)
+		
 		add_buttons(value.options)
 		
 		await get_tree().create_timer(0.5).timeout
 		%UI_dialog/PanelContainer/VBoxContainer/Options.show()
+
+var current_speaker = null
 
 
 #func _ready():
@@ -38,6 +48,11 @@ func hide_dialogue():
 func show_dialogue():
 	%UI_dialog.show()
 
+func add_quest(quest):
+	if QuestManager.quest == quest:
+		return
+	var button = quest_button.instantiate()
+	button.quest = quest
+	%UI_dialog/PanelContainer/VBoxContainer/Options.add_child(button)
 		
-		
-		
+
