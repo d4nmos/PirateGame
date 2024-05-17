ef0534755155f024981e2f89baf1d6488639bc0d
HeroInTheBox <a02polina@gmail.com>
2024-05-17 20:50:56 +0800

    Update quest_button.gd

--- NPCs/quest_button.gd
+++ NPCs/quest_button.gd
@@ -1,10 +1,10 @@
 extends Button
-
+ 
 var quest : Quest:
 	set(value):
 		quest = value
-		text = value.title + " [QUEST]"
-
+		text = value.title + " [КВЕСТ]"
+ 
 func _on_pressed():
 	QuestManager.quest = quest
 	queue_free()
