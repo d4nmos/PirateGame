8f7315228c1396afa2064d06a66902a09a638748
HeroInTheBox <a02polina@gmail.com>
2024-05-17 20:49:35 +0800

    Update slot_data.gd

--- inventory/scripts/slot_data.gd
+++ inventory/scripts/slot_data.gd
@@ -6,6 +6,7 @@ const MAX_STACK_SIZE: int = 99
 @export var item_data: ItemData
 @export_range(1, MAX_STACK_SIZE) var quantity: int = 1: set = set_quantity
 
+
 func can_fully_merge_with(other_slot_data: SlotData) -> bool:
 	return item_data == other_slot_data.item_data \
 		and item_data.stackable \
@@ -25,8 +26,10 @@ func create_single_slot_data() -> SlotData:
 	quantity -= 1
 	return new_slot_data
 
-func set_quantity(value: int):
+
+func set_quantity(value: int) -> void:
 	quantity = value
 	if quantity > 1 and not item_data.stackable:
 		quantity = 1 
 		push_error("%s is not stackable, setting quantity to 1" % item_data.name)
+
