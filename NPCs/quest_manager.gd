e48a1471c0f839184bda29d8e7da09ab10109862
HeroInTheBox <a02polina@gmail.com>
2024-05-17 20:50:58 +0800

    Update quest_manager.gd

--- NPCs/quest_manager.gd
+++ NPCs/quest_manager.gd
@@ -1,11 +1,12 @@
 extends Node2D
-
-
+ 
 var quest : Quest:
 	set(value):
 		quest = value
-		
+ 
 		if value.objective == "Fetch":
-			%Label.text = "Find " + value.title
-
-
+			%Label.text = "-> " + value.description
+ 
+func next_quest():
+	DialogueManager.current_speaker.dialogue = quest.next_questline
+	%Label.text = ""
