d145b52bf8aff6c0003c860360c0cce454e37374
HeroInTheBox <a02polina@gmail.com>
2024-05-17 20:51:03 +0800

    Update test_quest.gd

--- NPCs/test_quest.gd
+++ NPCs/test_quest.gd
@@ -1,51 +1,66 @@
 extends Node3D
 
 @export var dialogue: Dialogue
+const BottlePickUp = preload("res://NPCs/bottle_pick_up.tscn")
 
 @export var highlight_material: StandardMaterial3D
 
-#@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var chest_meshinstance: MeshInstance3D = $sword_shield_gold
 @onready var chest_material: StandardMaterial3D = chest_meshinstance.mesh.surface_get_material(0)
 
-var is_open: bool = false
+var is_clear: bool = false
 
-func open() -> void:
-	is_open = true
+func _ready():
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property($OmniLight3D, 'light_energy', 0, 0)
+
+func add_sign() -> void:
+	var questSign = $Quest_sign
+	var questSign2 = $Quest_sign2
+	if questSign:
+		questSign.visible = true
+	if questSign2:
+		questSign2.visible = true
+		
+func remove_sign() -> void:
 	var questSign = $Quest_sign
 	var questSign2 = $Quest_sign2
 	if questSign:
 		questSign.visible = false
 	if questSign2:
 		questSign2.visible = false
-	#animation_player.play("Open")
+
 
 func add_highlight() -> void:
-	#chest_meshinstance.set_surface_override_material(0, chest_material.duplicate())
-	#chest_meshinstance.get_surface_override_material(0).next_pass = highlight_material
 	var tween: Tween = get_tree().create_tween()
 	tween.tween_property($OmniLight3D, 'light_energy', 7, 0.5)
 	tween.tween_property($OmniLight3D, 'light_energy', 0, 2)
 
 func remove_highlight() -> void:
-	pass
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property($OmniLight3D, 'light_energy', 0, 2)
 
-# Open the chest if unopened
 func _on_interactable_interacted(_interactor: Interactor) -> void:
+	var pick_up = BottlePickUp.instantiate()
+	if QuestManager.quest != null:
+		if QuestManager.quest.number == "1" and pick_up.slot_data.item_data.find:
+			QuestManager.next_quest()
+			pick_up.slot_data.item_data.find = false
+			
 	DialogueManager.dialogue = dialogue
-	#DialogueManager.current_speaker = self
+	DialogueManager.current_speaker = self
 	DialogueManager.show_dialogue()
-	if not is_open:
-		#remove_highlight()
-		#$Interactable.queue_free()
-		open()
+	if DialogueManager.dialogue.ends:
+		remove_sign()
 
-
-# Add white outline
 func _on_interactable_focused(_interactor: Interactor) -> void:
-	if not is_open:
-		add_highlight()
+	add_highlight()
+	DialogueManager.dialogue = dialogue
+	DialogueManager.current_speaker = self
+	if not DialogueManager.dialogue.ends:
+		add_sign()
+		
 
-# Remove white outline
 func _on_interactable_unfocused(_interactor: Interactor) -> void:
 	remove_highlight()
+	remove_sign()
