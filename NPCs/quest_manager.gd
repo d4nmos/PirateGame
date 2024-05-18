e1d891837c0f84efbbc3fb621949466479b0cfe8
HeroInTheBox <a02polina@gmail.com>
2024-05-18 10:36:09 +0800

    Update quest_manager.gd

--- NPCs/quest_manager.gd
+++ NPCs/quest_manager.gd
@@ -1,12 +1,14 @@
 extends Node2D
- 
+
 var quest : Quest:
 	set(value):
 		quest = value
- 
 		if value.objective == "Fetch":
-			%Label.text = "-> " + value.description
- 
+			%Quest_name.text = "-> " + value.description
+
 func next_quest():
 	DialogueManager.current_speaker.dialogue = quest.next_questline
-	%Label.text = ""
+	%Quest_name.text = ""
+
+func change_quest_color():
+	%Quest_name.label_settings.font_color = Color(0, 167, 0)
