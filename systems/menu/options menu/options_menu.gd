4a69b927b9b875941d5513f7d25ca811578d0080
HeroInTheBox <a02polina@gmail.com>
2024-06-01 00:39:10 +0800

    Update options_menu.gd

--- systems/menu/options menu/options_menu.gd
+++ systems/menu/options menu/options_menu.gd
@@ -11,5 +11,6 @@ func _ready():
 
 func on_exit_pressed() -> void:
 	exit_options_menu.emit()
+	SettingsSignalBus.emit_set_settings_dictionary(SettingsContainer.create_storage_dictionary())
 	set_process(false)
 	
