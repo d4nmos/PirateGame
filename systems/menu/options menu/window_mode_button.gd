9282633270f1881ace5a51860b2dded0b1c09758
HeroInTheBox <a02polina@gmail.com>
2024-06-01 00:39:13 +0800

    Update window_mode_button.gd

--- systems/menu/options menu/window_mode_button.gd
+++ systems/menu/options menu/window_mode_button.gd
@@ -12,13 +12,19 @@ const WINDOW_MODE_ARRAY : Array[String] = [
 func _ready():
 	add_window_mode_items()
 	option_button.item_selected.connect(on_window_mode_selected)
+	load_data()
 
 func add_window_mode_items() -> void:
 	for window_mode in WINDOW_MODE_ARRAY:
 		option_button.add_item(window_mode)
 	
-	
+func load_data() -> void:
+	on_window_mode_selected(SettingsContainer.get_window_mode_index())
+	option_button.select(SettingsContainer.get_window_mode_index())
+
 func on_window_mode_selected(index: int) -> void:
+	SettingsSignalBus.emit_on_window_mode_selected(index)
+	
 	match index:
 		0: 
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
